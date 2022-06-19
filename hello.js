59512757348ce741b8d925b7a06835c614490b79
Munna-Mahmud <learnermunna@gmail.com>
2022-06-19 13:11:51 +0600

    hello world finished

--- hello.js
+++ hello.js
@@ -2,5 +2,13 @@
 // console.log(something here);
 //every programming language has different different way to see the output in terminal example: in python we write print(somthing here)
 
-// first code 
+// first code console a string
 console.log('Hello world');
+console.log('We Love America');
+
+console.log(87);
+// cocatination
+console.log('I am' + " " + 21 + 'years old');
+
+console.log(79+ 6);
+console.log('8' + 8)
