1405a9a24db8504a91508f9220012b94eea23a89
Munna-Mahmud <learnermunna@gmail.com>
2022-06-19 12:50:08 +0600

    first commit

---
+++ hello.js
@@ -0,0 +1,6 @@
+// //If we want to see any output in javaScript terminal We can simply write 
+// console.log(something here);
+//every programming language has different different way to see the output in terminal example: in python we write print(somthing here)
+
+// first code 
+console.log('Hello world');
