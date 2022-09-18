4bede67ba68fe9fb87cc5b8e21020111c3586ab3
Munna-Mahmud <learnermunna@gmail.com>
2022-09-19 01:36:08 +0600

    filter

--- Array/array.js
+++ Array/array.js
@@ -10,3 +10,15 @@
 var rollNumber = [34, 54, 87, 23, 12, 87, 100, 23, 55]
 
 // [] it's called square brackets 
+
+
+// get the first element on an array 
+getFirstValue([1, 2, 3]) 
+
+getFirstValue([80, 5, 100]) 
+
+getFirstValue([-500, 0, 50]) 
+
+function getFirstValue(arr) {
+	return arr[0]
+}
